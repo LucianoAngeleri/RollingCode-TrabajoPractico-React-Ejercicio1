c36420489e2a6535de8dbbc8d9e0551d30b249b7
Luciano Angeleri <lcnang@gmail.com>
2023-05-23 15:30:33 -0300

    Se creó el componente Titulo para mostrar el Hello World

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Titulo from "./components/Titulo.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <>
+    <Titulo></Titulo>
     </>
   )
 }
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,8 @@
+const Titulo = () => {
+    return (
+        <section className="container p-5 border rounded-5 text-bg-primary">
+            <h1 className='display-1'>Hello World!</h1>            
+        </section>
+    );
+};
+export default Titulo;
